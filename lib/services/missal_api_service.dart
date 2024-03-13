43822f00dab2c12436e3cea9f0f56d50f5dabcf8
jk <jan.kukorowski@wp.pl>
2024-03-13 14:58:26 +0100

    database setup & saving rubrics in the database

--- lib/services/missal_api_service.dart
+++ lib/services/missal_api_service.dart
@@ -1,12 +1,29 @@
 import 'dart:convert';
-
 import 'package:http/http.dart';
 import 'package:missale/models/calendar.dart';
 import 'package:missale/models/ordo.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class MissalApiService{
 
   String apiUrl = 'https://missalemeum.com/pl/api/v5';
+  late Database database;
+
+  databaseSetup() async {
+    var databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'missale.db');
+    database = await openDatabase(
+      path, version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute('CREATE TABLE rubrics (id TEXT PRIMARY KEY, json TEXT)');
+      }
+    );
+  }
+
+  MissalApiService(){
+    databaseSetup();
+  }
 
   Future<List<LiturgicalCalendar>> fetchCurrentYearCalendar() async {
     final response = await get(Uri.parse('$apiUrl/calendar'));
@@ -19,9 +36,19 @@ class MissalApiService{
   }
 
   Future<Ordo> fetchOrdo() async {
-    final response = await get(Uri.parse('$apiUrl/ordo'));
-    if(response.statusCode == 200){
-      return Ordo.fromJson((jsonDecode(response.body) as List)[0] as Map<String,dynamic>);
+    String json = '';
+    final queryResult = await database.query('rubrics', where: 'id=?', whereArgs: ['/ordo']);
+    if(queryResult.isNotEmpty){
+      json = queryResult.first['json'] as String;
+    } else {
+      final response = await get(Uri.parse('$apiUrl/ordo'));
+      if(response.statusCode == 200){
+        json = response.body;
+        database.insert('rubrics', {'id' : '/ordo', 'json': json}, conflictAlgorithm: ConflictAlgorithm.replace);
+      }
+    }
+    if(json.isNotEmpty){
+      return Ordo.fromJson((jsonDecode(json) as List)[0] as Map<String,dynamic>);
     }
     throw Exception();
   }
